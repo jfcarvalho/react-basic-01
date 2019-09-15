7f653205a5a20a8e6931af149077d34c943c4ecd
Jean Carvalho <jfcarvalho@dcc.ufba.br>
2019-09-15 19:34:19 -0300

    Mais manipulações utilizando o bootstrap

--- module01/confusion/src/App.js
+++ module01/confusion/src/App.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import './App.css';
+import React, {Component} from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 
-function App() {
+
+
+class App extends Component {
+ 
+  render() {
   return (
-    <div className="App">
+    <div>
     <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
     </Navbar>
+    <Menu />
     </div>
   );
+ }
 }
 
 export default App;
---
+++ module01/confusion/src/components/MenuComponent.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import { Media } from 'reactstrap';
+
+class Menu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes:[
+                {
+                    id: 0,
+                    name: 'Uthappizza',
+                    image: 'assets/images/uthappizza.png',
+                    category: 'mains',
+                    label: 'Hot',
+                    price:'4.99',
+                    description: 'Blablabla'
+
+                },
+                {
+                    id: 1,
+                    name: 'Zucchipakoda',
+                    image: 'assets/images/zucchipakoda.png',
+                    category: 'appetizer',
+                    label: '',
+                    price:'1.99',
+                    description: 'Blebleble'
+                },
+                {
+                    id: 2,
+                    name: 'Vadonut',
+                    image: 'assets/images/vadonut.png',
+                    category: 'appetizer',
+                    label: 'New',
+                    price:'1.99',
+                    description: 'Bliblibli'
+                },
+                {
+                    id: 3,
+                    name: 'ElaiCheese Cake',
+                    image: 'assets/images/elaicheesecake.png',
+                    category: 'dessert',
+                    label: '',
+                    price:'2.99',
+                    description: 'Blublublu'
+                }
+            ]
+        }
+    }
+
+    render () {
+        const menu = this.state.dishes.map((dish) => {
+            return (
+                <div key={dish.id} className="col-12 mt-5">
+                    <Media tag="li">
+                        <Media left middle>
+                        <Media object src={dish.image} alt={dish.name}/> 
+                        </Media>
+                        <Media body className="ml-5">
+                            <Media heading>{dish.name}</Media>
+                            <p>{dish.description}</p>
+                        </Media>   
+                    </Media>
+                </div>
+            );
+        });
+
+        return (
+            <div className="container">
+                <div className="row">
+                    <Media list>
+                        {menu}
+                    </Media>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Menu;
