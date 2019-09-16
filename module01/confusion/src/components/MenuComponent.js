503d2c67f1eff8663ca8469c0e8546a72176179c
Jean Carvalho <jfcarvalho@dcc.ufba.br>
2019-09-15 21:51:18 -0300

    Props e Eventos

--- module01/confusion/src/components/MenuComponent.js
+++ module01/confusion/src/components/MenuComponent.js
@@ -1,65 +1,49 @@
 import React, { Component } from 'react';
-import { Media } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle ,Media } from 'reactstrap';
 
 class Menu extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            dishes:[
-                {
-                    id: 0,
-                    name: 'Uthappizza',
-                    image: 'assets/images/uthappizza.png',
-                    category: 'mains',
-                    label: 'Hot',
-                    price:'4.99',
-                    description: 'Blablabla'
+            selectedDish: null
+        }
+    }
 
-                },
-                {
-                    id: 1,
-                    name: 'Zucchipakoda',
-                    image: 'assets/images/zucchipakoda.png',
-                    category: 'appetizer',
-                    label: '',
-                    price:'1.99',
-                    description: 'Blebleble'
-                },
-                {
-                    id: 2,
-                    name: 'Vadonut',
-                    image: 'assets/images/vadonut.png',
-                    category: 'appetizer',
-                    label: 'New',
-                    price:'1.99',
-                    description: 'Bliblibli'
-                },
-                {
-                    id: 3,
-                    name: 'ElaiCheese Cake',
-                    image: 'assets/images/elaicheesecake.png',
-                    category: 'dessert',
-                    label: '',
-                    price:'2.99',
-                    description: 'Blublublu'
-                }
-            ]
+    onDishSelect(dish)
+    {
+        this.setState({selectedDish:dish});
+    }
+
+    renderDish(dish) {
+        if(dish != null) {
+            return(
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardTitle>{dish.description}</CardTitle>
+                    </CardBody>
+                 </Card>   
+            );
+        }
+        else {
+            return (
+                <div></div>
+            );
         }
     }
 
     render () {
-        const menu = this.state.dishes.map((dish) => {
+        const menu = this.props.dishes.map((dish) => {
             return (
-                <div key={dish.id} className="col-12 mt-5">
-                    <Media tag="li">
-                        <Media left middle>
-                        <Media object src={dish.image} alt={dish.name}/> 
-                        </Media>
-                        <Media body className="ml-5">
-                            <Media heading>{dish.name}</Media>
-                            <p>{dish.description}</p>
-                        </Media>   
-                    </Media>
+                <div key={dish.id} className="col-12 col-md-5 m-1">
+                    <Card onClick={() => this.onDishSelect(dish)}>
+                        <CardImg width="100%" src={dish.image} alt={dish.name}/>             
+                            <CardImgOverlay body className="ml-5">
+                                <CardTitle>{dish.name}</CardTitle>
+                                <p>{dish.description}</p>
+                            </CardImgOverlay>   
+                    </Card>
                 </div>
             );
         });
@@ -67,9 +51,10 @@ class Menu extends Component {
         return (
             <div className="container">
                 <div className="row">
-                    <Media list>
                         {menu}
-                    </Media>
+                </div>
+                <div className="row">
+                    {this.renderDish(this.state.selectedDish)}
                 </div>
             </div>
         );
