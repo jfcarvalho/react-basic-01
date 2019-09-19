8864964c5f76288846317c886af70c0189f210d2
Purificacao <jean.purificacao@capgemini.com>
2019-09-19 14:53:47 -0300

    Subindo arquivos novos

---
+++ module02/confusion/src/components/AboutUSComponent.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class AboutUS extends Component {
+    render() {
+        return (
+        <h2>Isso aqui é um teste!!</h2>
+        );
+    }
+}
+
+export default AboutUS;
--- module02/confusion/src/components/MainComponent.js
+++ module02/confusion/src/components/MainComponent.js
@@ -3,6 +3,13 @@ import { Navbar, NavbarBrand } from 'reactstrap';
 import { DISHES } from '../shared/dishes';
 import Menu from '../components/MenuComponent';
 import DishDetail from '../components/DishDetailComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import AboutUS from './AboutUSComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+
 
 class Main extends Component {
 
@@ -25,19 +32,26 @@ class Main extends Component {
 
 
   render() {
+
+    const HomePage = () => {
+      return(
+          <Home 
+          />
+      );
+    }
+
     return (
       <div>
-      <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-      </Navbar>
-  
-      <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)}/>
-      
-       
-       <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} /> 
-          
+           
+      <Header />
+      <Switch>
+        <Route path='/home' component={HomePage} />
+        <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+        <Route exact path='/aboutus' component={() => <AboutUS/>} />
+
+        <Redirect to="/home" />
+      </Switch>
+       <Footer />   
       </div>
     );
    }
